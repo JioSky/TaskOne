01eb436912d395ddb5c6a682483ad6ffd29f525a
Jio Sky <135608317+JioSky@users.noreply.github.com>
2023-06-06 12:34:11 +0400

    Update TaskOneApplicationTests.java

--- src/test/java/HelloWorld/TaskOne/TaskOneApplicationTests.java
+++ src/test/java/HelloWorld/TaskOne/TaskOneApplicationTests.java
@@ -1,13 +1,21 @@
 package HelloWorld.TaskOne;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
-@SpringBootTest
-class TaskOneApplicationTests {
-
-	@Test
-	void contextLoads() {
+@SpringBootApplication
+public class TaskOneApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TaskOneApplication.class, args);
 	}
-
+	@RestController
+	public static class HelloController {
+		@GetMapping("/")
+		public Mono<String> getIndex() {
+			return Mono.just("Hello World!");
+		}
+	}
 }
