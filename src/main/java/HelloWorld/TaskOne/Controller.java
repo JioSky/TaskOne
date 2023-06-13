a3775bc29f499a22af1d76136f1361b78b3497a1
Jio Sky <135608317+JioSky@users.noreply.github.com>
2023-06-13 17:54:24 +0400

    Update and rename GreetingHandler.java to Controller.java

---
+++ src/main/java/HelloWorld/TaskOne/Controller.java
@@ -0,0 +1,13 @@
+package HelloWorld.TaskOne;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello world!";
+    }
+
+}
